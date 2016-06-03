496bd7062a16d4f7b2a95224e4617d89edcb8c1d
libertysyarn <code@libertysyarn.com>
2016-06-03 11:20:54 -0400

    setup board

--- js/index.js
+++ js/index.js
@@ -1,20 +1,34 @@
 var user = ['O', 'X'];
 var $bigBtn = $('.big-btn');
+var $alert = $('#alert-box');
+var $;
+var board = [];
+var turn = 0;
+var playerWins = 0;
+var aiWins = 0;
 
-$('#optionX').on('click', function() {
-  user.sort().reverse();
+$('#clearAll').on('click', function () {
+	$bigBtn.text('');
+	$bigBtn.button('reset');
 });
 
-$('#optionO').on('click', function() {
-  user.sort();
+$('#optionX').on('click', function () {
+	user.sort().reverse();
 });
 
-$bigBtn.on('click', function() {
-  var $btn = $(this).button();
-  $btn.text(user[0]);
+$('#optionO').on('click', function () {
+	user.sort();
 });
 
-$('#clearAll').on('click', function() {
-  $bigBtn.text('');
-  $bigBtn.button('reset');
-})
+$bigBtn.on('click', function () {
+	var $btn = $(this).button();
+	$btn.text(user[0]);
+	turn++;
+	console.log(turn);
+});
+
+// TODO mark space as used ? remove it from a list of options
+// TODO call machine to take turn
+// TODO stop when all spaces used
+// TODO function for machine to take turn
+// TODO first turn can go in any available space
